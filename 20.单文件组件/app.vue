4a115eca38f7e332ff5387a6503d88cb8d8d13da
MirageClouds <2159272804@qq.com>
2024-03-04 18:57:02 +0800

    Vue基础

---
+++ 20.单文件组件/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <school/>
+    <student/>
+  </div>
+</template>
+
+<script>
+import school from "./School.vue";
+import Student from "./student.vue";
+
+export default {
+  name: "app",
+  components: {Student},
+  computed: {
+    school,
+    student
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 20.单文件组件/main.js
@@ -0,0 +1,11 @@
+import app from "./app.vue";
+
+new Vue({
+	el: '#root',
+	components: {app},
+	template: `
+        <div>
+            <app></app>        
+        </div>
+    `
+})
