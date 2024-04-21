521f277fb46283d0c1776081db0f5cb13683cd1f
Mirage <2159272804@qq.com>
2024-04-21 23:31:01 +0800

    Vue基础

--- BookManager/book_manager/src/pages/book-info.vue
+++ BookManager/book_manager/src/pages/book-info.vue
@@ -70,7 +70,42 @@ export default {
       batchDeleteForm: [],
       // 是否批量删除
       ifOpenBacth: false,
-      ifBacthDelete: false
+      ifBacthDelete: false,
+
+      /*编辑图书信息部分*/
+      ifUpdataOpen: false,
+      updataFromDete: {
+        bookname: '',
+        bookauthor: '',
+        bookdesc: '',
+        bookid: '',
+        bookimg: '',
+        bookprice: '',
+        booktypeid: '',
+        booktypename: '',
+        isborrowed: null
+      },
+
+      /*删除单条数据部分*/
+      deleteRow: {
+        bookname: '',
+        bookauthor: '',
+        bookdesc: '',
+        bookid: '',
+        bookimg: '',
+        bookprice: '',
+        booktypeid: '',
+        booktypename: '',
+        isborrowed: null
+      },
+
+      /*借阅部分*/
+      ifBorrowBook: false,
+      userItem: [],
+      borrow: {
+        borrowid: null,
+        bookid: null
+      }
     }
   },
   methods: {
@@ -81,9 +116,9 @@ export default {
       cb(this.bookType);
     },
     // 修改条件查询数据的BookTypeId
-    updataSelectBookTypeId(item) {
+    updataSelectBookTypeId() {
       for (let i = 0; i < this.bookType.length; i++) {
-        if (item === this.bookType[i].value) {
+        if (this.selectBook.bookTypeName === this.bookType[i].value) {
           this.selectBook.bookTypeId = this.bookType[i].booktypeid;
         }
       }
@@ -166,11 +201,15 @@ export default {
             this.selectBook.bookName = ''
             this.selectBook.bookAuthor = ''
             this.selectBook.bookTypeName = ''
+            this.selectBook.bookTypeId = null
           },
           error => {
             console.log(error)
           }
       )
+    },
+    selectBookClear() {
+      this.selectBook.bookTypeId = null
     },
 
     /*添加图书部分*/
@@ -215,6 +254,9 @@ export default {
       console.log(file)
       this.$refs.upload.handleRemove(file)
     },
+    handleAvatarPreview(file) {
+      console.log(file)
+    },
     // 图片上传超过最大限制调用
     handleAvatarExceed() {
       this.$message.error('图片以超过最大上传上限，请删除后重新上传')
@@ -232,9 +274,16 @@ export default {
         isborrowed: 0
       }).then(
           () => {
-            this.$message('添加图书成功')
+            this.$message.success('添加图书成功')
             this.selectAllBookInfo()
             this.dialogAddBook = false
+            this.dialogFormAdd.bookname = ''
+            this.dialogFormAdd.bookauthor = ''
+            this.dialogFormAdd.bookTypeId = null
+            this.dialogFormAdd.bookImg = ''
+            this.dialogFormAdd.bookdesc = ''
+            this.dialogFormAdd.bookprice = ''
+            this.dialogFormAdd.bookTypeName = ''
           },
           error => {
             this.$message.error(error)
@@ -245,11 +294,20 @@ export default {
     /*批量删除部分*/
 
     // 批量删除时调用
-    // 判断是否发送axios请求
-    ifdeleteBatch() {
-      if (this.ifBacthDelete) {
+    // 判断是否确认删除
+    openDeleteAll() {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
         this.deleteBatch()
-      }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
     // 发送axios请求
     deleteBatch() {
@@ -282,6 +340,133 @@ export default {
     batchDeleteAllData(selection) {
       this.batchDeleteForm = selection
       console.log(this.batchDeleteForm)
+    },
+
+    /*编辑数据部分*/
+    handleEdit(index, row) {
+      console.log(index)
+      console.log(row)
+      this.updataFromDete.bookname = row.bookname
+      this.updataFromDete.bookid = row.bookid
+      this.updataFromDete.booktypeid = row.booktypeid
+      this.updataFromDete.bookauthor = row.bookauthor
+      this.updataFromDete.booktypename = row.booktypename
+      this.updataFromDete.bookprice = row.bookprice
+      this.updataFromDete.isborrowed = row.isborrowed
+      this.updataFromDete.bookimg = row.bookimg
+      this.updataFromDete.bookdesc = row.bookdesc
+    },
+    dialogFormUpdataSuccess() {
+      this.$axios.put('/api/bookInfo/updateBookInfo', {
+        bookname: this.updataFromDete.bookname,
+        bookauthor: this.updataFromDete.bookauthor,
+        booktypeid: this.updataFromDete.booktypeid,
+        bookdesc: this.updataFromDete.bookdesc,
+        bookprice: this.updataFromDete.bookprice,
+        isborrowed: this.updataFromDete.isborrowed,
+        bookimg: this.updataFromDete.bookimg,
+        bookid: this.updataFromDete.bookid,
+      }).then(
+          () => {
+            this.$message.success('修改图书信息成功')
+            this.selectAllBookInfo()
+            this.ifUpdataOpen = false
+          },
+          error => {
+            this.$message.error(error)
+          }
+      )
+    },
+
+    /*删除单条数据部分*/
+    handleDelete(index, row) {
+      this.deleteRow.bookname = row.bookname
+      this.deleteRow.bookid = row.bookid
+      this.deleteRow.booktypeid = row.booktypeid
+      this.deleteRow.bookauthor = row.bookauthor
+      this.deleteRow.booktypename = row.booktypename
+      this.deleteRow.bookprice = row.bookprice
+      this.deleteRow.isborrowed = row.isborrowed
+      this.deleteRow.bookimg = row.bookimg
+      this.deleteRow.bookdesc = row.bookdesc
+      this.openDelete()
+    },
+    openDelete() {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$axios.delete('/api/bookInfo/deleteBookInfo', {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            bookname: this.deleteRow.bookname,
+            bookauthor: this.deleteRow.bookauthor,
+            booktypeid: this.deleteRow.booktypeid,
+            bookdesc: this.deleteRow.bookdesc,
+            bookprice: this.deleteRow.bookprice,
+            isborrowed: this.deleteRow.isborrowed,
+            bookimg: this.deleteRow.bookimg,
+            bookid: this.deleteRow.bookid,
+          }
+        }).then(
+            res => {
+              if (res.status === 200) {
+                this.$message.success('删除成功')
+              } else {
+                this.$message.error('删除失败')
+              }
+              this.selectAllBookInfo()
+            },
+            error => {
+              this.$message.error(error)
+            }
+        )
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+
+    /*借阅图书部分*/
+    //获取用户信息
+    getUser() {
+      this.$axios.get('/api/user/queryUsers').then(
+          res => {
+            console.log(res.data)
+            for (let i = 0; i < res.data.length; i++) {
+              this.userItem.push(res.data[i])
+            }
+            console.log(this.userItem)
+          }
+      )
+    },
+    handleBorrow(...row) {
+      console.log(row)
+      this.getUser()
+      this.ifBorrowBook = true
+      this.borrow.bookid = row[1].bookid
+    },
+    dialogBorrowBookSuccess() {
+      this.$axios.post(`/api/borrow/borrowBook?userid=${this.borrow.borrowid}&bookid=${this.borrow.bookid}`).then(
+          res => {
+            if (res.data === 1) {
+              this.$message.success('借阅成功')
+            } else {
+              this.$message.error('借阅失败，请联系管理员')
+            }
+            this.selectAllBookInfo()
+            this.ifBorrowBook = false
+
+          },
+          error => {
+            this.$message.error(error)
+          }
+      )
     }
   },
   // 加载获取数据
@@ -318,14 +503,14 @@ export default {
   <div>
     <!--条件查询部分-->
     <el-row :gutter="20" style="width: 700px;">
-      <el-col :span="4">
+      <el-col :span="5">
         <el-input
             v-model="selectBook.bookName"
             clearable
             placeholder="书名">
         </el-input>
       </el-col>
-      <el-col :span="4">
+      <el-col :span="5">
         <el-input
             v-model="selectBook.bookAuthor"
             clearable
@@ -339,6 +524,7 @@ export default {
             class="inline-input"
             clearable
             placeholder="请输入内容"
+            @clear="selectBookClear"
             @input=updataSelectBookTypeId
         ></el-autocomplete>
       </el-col>
@@ -349,29 +535,20 @@ export default {
         <el-button icon="el-icon-search" type="primary" @click="selectAllBookInfo">显示全部</el-button>
       </el-col>
     </el-row>
+
+
     <!--添加图书和批量删除部分-->
     <el-row :gutter="20" style="width: 400px; margin-top: 20px">
       <el-col :span="8">
         <el-button icon="el-icon-edit" type="primary" @click="dialogAddBook = !dialogAddBook">添加图书</el-button>
       </el-col>
       <el-col :span="8">
-        <el-button icon="el-icon-delete" type="danger" @click="ifOpenBacth = true; ifBacthDelete = true">批量删除
+        <el-button icon="el-icon-delete" type="danger" @click="openDeleteAll">批量删除
         </el-button>
       </el-col>
     </el-row>
-    <!--批量删除弹窗-->
-    <el-dialog
-        :visible.sync="ifOpenBacth"
-        title="提示"
-        width="30%">
-      <span>
-        <i class="el-icon-question"></i>
-        确定要删除这些记录吗?</span>
-      <span slot="footer" class="dialog-footer">
-    <el-button @click="ifOpenBacth = false;ifBacthDelete = false">取 消</el-button>
-    <el-button type="primary" @click="ifdeleteBatch">确 定</el-button>
-  </span>
-    </el-dialog>
+
+
     <!--添加图书弹窗-->
     <el-dialog
         :visible.sync="dialogAddBook"
@@ -411,6 +588,7 @@ export default {
               :before-upload="beforeAvatarUpload"
               :limit="1"
               :on-exceed="handleAvatarExceed"
+              :on-preview="handleAvatarPreview"
               :on-success="handleAvatarSuccess"
               action="http://localhost:8092/BookManager/update/updateImg" class="avatar-uploader"
               list-type="picture-card"
@@ -449,6 +627,8 @@ export default {
         <el-button type="primary" @click="dialogFormAddSuccess">确 定</el-button>
       </span>
     </el-dialog>
+
+
     <!--数据列表部分-->
     <el-table
         :data="bookInfo"
@@ -524,7 +704,7 @@ export default {
           <el-button
               size="mini"
               type="primary"
-              @click="handleEdit(scope.$index, scope.row)">编辑
+              @click="handleEdit(scope.$index, scope.row),ifUpdataOpen=true">编辑
           </el-button>
           <el-button
               size="mini"
@@ -534,11 +714,112 @@ export default {
           <el-button
               size="mini"
               type="success"
-              @click="handleDelete(scope.$index, scope.row)">借阅图书
+              @click="handleBorrow(scope.$index, scope.row)">借阅图书
           </el-button>
         </template>
       </el-table-column>
     </el-table>
+
+
+    <!--编辑图书信息弹窗-->
+    <el-dialog
+        :visible.sync="ifUpdataOpen"
+        title="提示"
+        width="30%">
+      <el-container>
+        <el-aside>
+          <el-form ref="ruleForm" :model="updataFromDete" :rules="ruleFrom" class="demo-ruleForm" label-width="100px">
+            <el-form-item label="图书名称" prop="bookname">
+              <el-input v-model="updataFromDete.bookname"></el-input>
+            </el-form-item>
+            <el-form-item label="作者" prop="bookauthor">
+              <el-input v-model="updataFromDete.bookauthor"></el-input>
+            </el-form-item>
+            <el-form-item label="价格" prop="bookprice">
+              <el-input v-model="updataFromDete.bookprice"></el-input>
+            </el-form-item>
+            <el-form-item label="图书类型" prop="bookTypeId">
+              <el-autocomplete
+                  v-model="updataFromDete.booktypename"
+                  :fetch-suggestions="querySearch"
+                  class="inline-input"
+                  clearable
+                  placeholder="请输入图书类型"
+                  @input=updataDialogFormAddBookTypeId
+              ></el-autocomplete>
+            </el-form-item>
+            <el-form-item label="图书描述" prop="bookdesc">
+              <el-input v-model="updataFromDete.bookdesc" type="textarea"></el-input>
+            </el-form-item>
+          </el-form>
+        </el-aside>
+        <el-main>
+          <h2>点击上传图书封面</h2>
+          <el-upload
+              ref="upload"
+              :before-upload="beforeAvatarUpload"
+              :limit="1"
+              :on-exceed="handleAvatarExceed"
+              :on-success="handleAvatarSuccess"
+              action="http://localhost:8092/BookManager/update/updateImg"
+              class="avatar-uploader"
+              list-type="picture-card"
+          >
+            <i slot="default" class="el-icon-plus"></i>
+            <div slot="file" slot-scope="{file}">
+              <img
+                  v-if="dialogFormAdd.bookImg"
+                  :src="dialogFormAdd.baseApi + updataFromDete.bookimg "
+                  alt="" class="el-upload-list__item-thumbnail avatar"
+              >
+              <span class="el-upload-list__item-actions">
+                <span
+                    class="el-upload-list__item-preview"
+                    @click="handlePictureCardPreview()"
+                >
+                  <i class="el-icon-zoom-in"></i>
+                </span>
+                <span
+                    v-if="!disabled"
+                    class="el-upload-list__item-delete"
+                    @click="handleRemove(file)"
+                >
+                  <i class="el-icon-delete"></i>
+                </span>
+              </span>
+              <el-dialog :append-to-body='true' :visible.sync="dialogVisible">
+                <img :src="dialogFormAdd.baseApi + updataFromDete.bookimg" alt="" width="100%">
+              </el-dialog>
+            </div>
+          </el-upload>
+        </el-main>
+      </el-container>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="ifUpdataOpen = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormUpdataSuccess">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!--借阅图书弹窗-->
+    <el-dialog
+        :visible.sync="ifBorrowBook"
+        title="提示"
+        width="30%"
+    >
+      <el-form ref="form" :model="borrow" label-width="80px">
+        <el-form-item label="活动区域">
+          <el-select v-model="borrow.borrowid" placeholder="请选择用户">
+            <el-option v-for="(i,item) in this.userItem" :key="item" :label="i.username"
+                       :value=" i.userid "></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="ifBorrowBook = false">取 消</el-button>
+        <el-button type="primary" @click="dialogBorrowBookSuccess">确 定</el-button>
+      </span>
+    </el-dialog>
+
     <!--分页组件部分-->
     <el-pagination
         :current-page.sync="pageBook.currentPage"
