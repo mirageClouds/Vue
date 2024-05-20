aa5b5fd529b134c744ca7018a897d0cf5874a72e
Mirage <2159272804@qq.com>
2024-05-20 17:19:08 +0800

    echarts练习

--- echarts/echarts-vue3/src/compoent/bar.vue
+++ echarts/echarts-vue3/src/compoent/bar.vue
@@ -1,42 +1,56 @@
 <script setup>
 import {ref,onMounted} from 'vue'
 import axios from 'axios'
+import * as echarts from 'echarts'
 
 
 const listData = {}
-const reData = []
+const reData = {
+  name:[],
+  value:[]
+}
 
 const getList = ()=>{
   axios.get('https://mock.mengxuegu.com/mock/6641ce0d37199f49537c36d0/example/test23').then(
       res=>{
         let data = res.data.data.orders
         data.forEach(item=>{
-          console.log(item)
-          if(listData[item.regionName]){
-            item.details.forEach(i=>{
-              if(listData[item.regionName][i.quantity]){
-                listData[item.regionName][i.quantity].price += i.price
-              }else {
-                listData[item.regionName][i.quantity].quantity = {price:i.price}
-              }
-            })
-            listData[item.regionName].finalTotalAmount += item.finalTotalAmount
-          }else {
-            listData[item.regionName] = {finalTotalAmount:item.finalTotalAmount}
-            item.details.forEach(i=>{
-              if(listData[item.regionName][i.quantity]){
-                listData[item.regionName][i.quantity].price += i.price
-              }else {
-                listData[item.regionName][i.quantity].quantity = {price:i.price}
-              }
-            })
+          let regionName = item.regionName
+          if(!listData[regionName]){
+            listData[regionName] = {
+              finalTotalAmount:0,
+              details:{}
+            }
           }
+
+          listData[regionName].finalTotalAmount += item.finalTotalAmount
+
+          let details = item.details
+          details.forEach(i=>{
+            let quantity = i.quantity
+            if (!listData[regionName].details[quantity]){
+              listData[regionName].details[quantity] = {
+                item:i.item,
+                price:i.price
+              }
+            }
+          })
         })
 
-        for (let i in listData){
+        let arr = []
 
-          reData.push({name:i,value:listData[i].quantity,value1:listData[i].finalTotalAmount.toFixed(2)})
-          console.log(reData)
+        for (let i in listData){
+          arr.push({
+            finalTotalAmount: listData[i].finalTotalAmount.toFixed(2),
+            name:i,
+            details:listData[i].details
+          })
+        }
+        arr = arr.sort((a,b)=>b.finalTotalAmount-a.finalTotalAmount)
+        arr = arr.splice(0,5)
+        for(let i in arr){
+          reData.name.push(arr[i].name)
+          reData.value.push(Number(arr[i].finalTotalAmount))
         }
 
       }
@@ -45,10 +59,39 @@ const getList = ()=>{
 
 getList()
 
+
+const MyEcharts = ()=>{
+  console.log(reData.name)
+  let myChart = echarts.init(document.getElementById('tb'))
+  let option = {
+    xAxis:{
+      type:'value',
+
+    },
+    yAxis:{
+      data:reData.name
+    },
+    series:[
+      {
+        type:'bar',
+        data:reData.value
+      }
+    ]
+  }
+  myChart.setOption(option)
+}
+
+onMounted(()=>{
+  setTimeout(()=>{
+    MyEcharts()
+  },500)
+})
+
+
 </script>
 
 <template>
-
+<div id="tb" style="width: 1000px;height: 500px;"></div>
 </template>
 
 <style scoped lang="scss">
